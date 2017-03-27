c8ea0b3f1f85e78d6d1d9272679a7743fd069f1b
debitop <89263693878@mail.ru>
2017-03-27 23:56:42 +0200

    Add_new_task_05

---
+++ src/com/company/Lesson21_OOP/Test05/Cat.java
@@ -0,0 +1,16 @@
+package com.company.Lesson21_OOP.Test05;
+
+/**
+ * Created by Саша on 27.03.2017.
+ */
+/* Ничего не поменяешь
+1. Создать класс Cat с переменными name, age, weight.
+2. Скрыть все внутренние переменные класса Cat:
+- создать геттеры и сеттеры
+- скрыть методы, позволяющие менять внутреннее состояние объектов класса Cat.
+*/
+public class Cat {
+    private String name;
+    private int age;
+    private int weight;
+}
