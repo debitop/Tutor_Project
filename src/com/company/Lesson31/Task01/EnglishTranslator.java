e3faefc7effac76991842ee994e0bbea03a2b204
debitop <89263693878@mail.ru>
2017-04-04 22:27:06 +0200

    Add_new_task_09

---
+++ src/com/company/Lesson31/Task01/EnglishTranslator.java
@@ -0,0 +1,23 @@
+package com.company.Lesson31.Task01;
+
+/**
+ * Created by Саша on 04.04.2017.
+ */
+/*
+1. Создать класс EnglishTranslator, который наследуется от абстрактного класса Translator.
+   В классе Translator создать абстрактный метод getLanguage() и не абстрактный метод
+    public String translate(), который возвращает строку "Я переводчик с " + getLanguage();
+2. Реализовать все абстрактные методы.
+3. Подумай, что должен возвращать метод getLanguage.
+4. Программа должна выводить на экран "Я переводчик с английского".
+5. Создать класс GermanyTranslator, который наследуется от абстрактного класса Translator.
+6. Реализовать все абстрактные методы.
+7. Подумай, что должен возвращать метод getLanguage.
+8. Программа должна выводить на экран "Я переводчик с немецкого".
+*/
+public class EnglishTranslator extends Translator{
+    @Override
+    protected String getLanguage() {
+        return "English";
+    }
+}
