25ade0170ed1dfb0ae89dc506a15baf296de4375
debitop <89263693878@mail.ru>
2017-04-05 23:23:51 +0200

    Add_new_task_10

---
+++ src/com/company/Lesson31/Task01/GermanyTranslator.java
@@ -0,0 +1,11 @@
+package com.company.Lesson31.Task01;
+
+/**
+ * Created by Саша on 04.04.2017.
+ */
+public class GermanyTranslator extends Translator {
+    @Override
+    protected String getLanguage() {
+        return "Germany";
+    }
+}
