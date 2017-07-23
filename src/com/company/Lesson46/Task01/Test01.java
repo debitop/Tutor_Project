b0d954e9e55e4ade734086e569023164c433350e
debitop <89263693878@mail.ru>
2017-07-23 23:48:18 +0200

    Add_new_task_24

---
+++ src/com/company/Lesson46/Task01/Test01.java
@@ -0,0 +1,17 @@
+package com.company.Lesson46.Task01;
+
+public class Test01 {
+
+    public static void main(String[] args) {
+        TestThread task = new TestThread();
+        new Thread(task).start();
+    }
+
+    public static class TestThread implements Runnable {
+        @Override
+        public void run() {
+            System.out.println("My first thread");
+        }
+    }
+
+}
---
+++ src/com/company/Lesson46/Test01.java
@@ -0,0 +1,42 @@
+package com.company.Lesson46;
+
+public class Test01 {
+
+    public static void main(String[] args) {
+        E e = new E();
+        T t = new T();
+        Y y = new Y();
+
+        t=y;
+       // t=e;
+       // t=(T)e; classCast
+       // t=(E)e; ошибка компил
+       // t=(Y)y;
+        t=(T)t;
+
+        System.out.println(e);
+    }
+}
+
+class E {
+    public void m() {
+
+    }
+}
+
+class T extends E {
+    public void m1() {
+
+    }
+}
+
+class Y extends T {
+    public void m2() {
+
+    }
+
+
+
+
+
+}
