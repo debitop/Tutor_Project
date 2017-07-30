edbc26df05fca292c77ab53b2f75c8119d5cfe0c
debitop <89263693878@mail.ru>
2017-07-30 22:21:50 +0200

    Add_new_task_24

---
+++ src/com/company/Interview/Factorial.java
@@ -0,0 +1,31 @@
+package com.company.Interview;
+
+import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Factorial {
+    public static void main(String[] args) {
+        System.out.println(factorial(6500));
+        System.out.println(factorial(6000));
+    }
+
+    static Map<Integer, BigInteger> cache = new HashMap<>();
+
+    private static BigInteger factorial(int n){
+        BigInteger result;
+
+        if(n == 0) return BigInteger.ONE;
+
+//        if((result = cache.get(n)) != null){
+//            return result;
+//        }
+        if(cache.containsKey(n)){
+            System.out.println("Cache");
+            return cache.get(n);
+        }
+        result = BigInteger.valueOf(n).multiply(factorial(n - 1));
+        cache.put(n, result);
+        return result;
+    }
+}
---
+++ src/com/company/Interview/Factorial_test.java
@@ -0,0 +1,27 @@
+package com.company.Interview;
+
+import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Factorial_test {
+    public static void main(String[] args) {
+        System.out.println(facrorial(10));
+    }
+
+    static Map<Integer, BigInteger> cache = new HashMap<>();
+
+    private static BigInteger facrorial(int x) {
+        BigInteger result;
+        if (x == 0) return BigInteger.ONE;
+
+
+        if (cache.containsKey(x)) {
+            System.out.println("cache");
+            return cache.get(x);
+        }
+        result = BigInteger.valueOf(x).multiply(facrorial(x - 1));
+        cache.put(x,result);
+        return result;
+    }
+}
