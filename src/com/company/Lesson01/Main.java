323146588aa6c6630794a81f2762fc0a5481c111
debitop <89263693878@mail.ru>
2017-10-01 22:02:15 +0200

    Add_new_task_25

--- src/com/company/Lesson01/Main.java
+++ src/com/company/Lesson01/Main.java
@@ -2,6 +2,7 @@ package com.company.Lesson01;
 
 public class Main {
     public static void main(String[] args) {
+        // TODO: 01.10.2017 message - ffff
         int a = 1; // Integer
         a = a + 5;
         int q = 12;
