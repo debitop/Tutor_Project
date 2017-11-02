1cbd7664c81c31c9c87c73cde3ceaef65052ca48
debitop <89263693878@mail.ru>
2017-11-02 22:11:24 +0200

    Add_new_task_25

---
+++ src/com/company/Lesson57_Patterns_Factory_Method/Task01/common/JpgReader.java
@@ -0,0 +1,19 @@
+package com.company.Lesson57_Patterns_Factory_Method.Task01.common;
+
+import com.company.Lesson57_Patterns_Factory_Method.Task01.ImageReader;
+
+/* Factory method pattern
+1. Создай enum класс ImageTypes с списоком доступных форматов BMP, JPG, PNG;
+1. Создай интерфейс-маркер ImageReader
+2. В отдельных файлах в пакете common создай классы JpgReader, PngReader, BmpReader,
+которые реализуют интерфейс ImageReader.
+3. Создай класс ImageReaderFactory с одним методом.
+3.1. Подумай, как он должен называться.
+3.2. Подумай, какие модификаторы должны быть у этого метода.
+4. Этот метод должен:
+4.1. для каждого значения из ImageTypes возвращать соответствующий Reader, например, для ImageTypes.JPG - JpgReader;
+4.2. если передан неправильный параметр, то выбросить исключение IllegalArgumentException("Неизвестный тип картинки").
+5. Реализовать метод main. Запустить программу.
+*/
+public class JpgReader implements ImageReader {
+}
