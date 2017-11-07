d9e8d174a0406e084cea8d43aa56b00c2e8f1216
debitop <89263693878@mail.ru>
2017-11-07 22:58:35 +0200

    Add_new_task_25

---
+++ src/com/company/HomeWork/Test04_factorial.java
@@ -0,0 +1,31 @@
+package com.company.HomeWork;
+
+import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Test04_factorial {
+    public static void main(String[] args) {
+        System.out.println(fact(10));
+        System.out.println(fact(6));
+
+
+        //System.out.println(fact());
+
+    }
+   static Map <Integer,BigInteger> cache = new HashMap<>();
+    static BigInteger fact(int num){
+        BigInteger fact=BigInteger.ONE;
+        if (num==0){
+             return BigInteger.ONE;
+        }
+        if (cache.containsKey(num)) {
+            System.out.print("cache: ");
+           return cache.get(num);
+
+        } else
+        fact = BigInteger.valueOf(num).multiply(fact(num-1));
+        cache.put(num,fact);
+        return fact;
+    }
+}
