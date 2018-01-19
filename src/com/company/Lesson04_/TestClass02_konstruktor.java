a90d3ce5c93e06277c8c2ad494403d54b0a81d8f
debitop <89263693878@mail.ru>
2018-01-19 07:54:27 +0200

    Add_new_task_25

--- src/com/company/Lesson04_/TestClass02_konstruktor.java
+++ src/com/company/Lesson04_/TestClass02_konstruktor.java
@@ -3,7 +3,7 @@ package com.company.Lesson04_;
 /**
  * Created by Саша on 01.11.2016.
  */ // Cat -> name, age, tail; main -> 2 object -> 1 - sout, 2 - toString
-public class TestClass02 {
+public class TestClass02_konstruktor {
     public static void main(String[] args) {
         Cat cat1 = new Cat("Барсик", 5, 10);
         Cat cat2 = new Cat("Барс", 3, 7);
